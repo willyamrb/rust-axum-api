f7091f90361380eee279893fc96c27f9dde6510f
willyamrb <will@atom6studio.com>
2024-04-13 12:13:52 -0300

    working with body and json

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,3 +1,19 @@
+use axum::Json;
+use serde::{Deserialize, Serialize};
+
 pub async fn index() -> &'static str {
     "Hello, World!"
+}
+
+pub async fn body_test(body: String) -> String {
+    body
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Mirror {
+    message: String,
+}
+
+pub async fn body_json_test(Json(body): Json<Mirror>) -> Json<Mirror> {
+    Json(body)
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,12 @@
-use crate::controllers::index;
-use axum::{routing::get, Router};
+use crate::controllers::{body_json_test, body_test, index};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(index))
+    Router::new()
+        .route("/", get(index))
+        .route("/body", post(body_test))
+        .route("/json", post(body_json_test))
 }
